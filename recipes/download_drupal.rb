89df953b90cd81f338950a18e553527e0433d606
David Kinzer <dtkinzer@gmail.com>
2014-07-31 00:10:13 -0400

    Fix drupal download source.

--- recipes/download_drupal.rb
+++ recipes/download_drupal.rb
@@ -2,7 +2,7 @@
 ## Recipe:: download_drupal
 ##
 ## download drupal.
-version = "#{node['deploy-drupal']['version']}.#{node['deploy-deploy']['minor_version']}"
+version = "#{node['deploy-drupal']['version']}.#{node['deploy-drupal']['minor_version']}"
 tmp_dir = Chef::Config[:file_cache_path]
 repo_url = "http://ftp.drupal.org/files/projects"
 
@@ -14,18 +14,16 @@ remote_file "#{tmp_dir}/drupal-#{version}.tar.gz" do
   source "#{repo_url}/drupal-#{version}.tar.gz"
   mode 0644
   action "create_if_missing"
-  notifies "run", "execute[untar-drupal]", "immediately"
 end
 
 execute "untar-drupal" do
   cwd tmp_dir
-  command "tar -xzf drupal-#{version}.tar.gz -C #{node['deploy-drupal']['project_name']}/site --strip-components=1"
-  action "nothing"
-  notifies "run", "execute[install-project-from-path]", "immediately"
+  command "tar -xzf drupal-#{version}.tar.gz -C #{node['deploy-drupal']['project_name']} --strip-components=1"
+  not_if { File.exists? "#{node['deploy-drupal']['drupal_root']}/index.php" }
 end
 
 execute "install-project-from-path" do
-  command "cp -Rf '#{node['deploy-drupal']['get_project']['path']}/.' '#{node['deploy-drupal']['project_root']}'"
+  command "cp -Rf '#{tmp_dir}/#{node['deploy-drupal']['project_name']}/.' '#{node['deploy-drupal']['project_root']}'"
   group node['deploy-drupal']['dev_goup']
   creates node['deploy-drupal']['drupal_root'] + "/index.php"
   not_if { File.exists? "#{node['deploy-drupal']['drupal_root']}/index.php" }
